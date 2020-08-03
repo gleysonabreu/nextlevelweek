22a42b2a39f7be66cc11bfd721531751f79ae5d3
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-08-03 20:11:48 -0300

    Add responsive on TeacherForm page.

--- web/src/pages/TeacherForm/styles.ts
+++ web/src/pages/TeacherForm/styles.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const PageTeacherForm = styled.div`
   width: 100vw !important;
   height: 100vh;
+
+  @media(min-width: 700px){
+    max-width: 100% !important;
+  }
 `;
