f0e95c9167618bff6b1a3374c267b24433e9989e
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-08-03 20:09:41 -0300

    Change theme colors.

--- web/src/assets/styles/GlobalStyles.ts
+++ web/src/assets/styles/GlobalStyles.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
   :root{
-    --color-background: #F0F0F7;
+    --color-background: #13111B;
     --color-primary-lighter: #9871F5;
     --color-primary-light: #916BEA;
     --color-primary: #8257E5;
@@ -12,14 +12,14 @@ const GlobalStyles = createGlobalStyle`
     --color-secundary-dark: #04BF58;
     --color-title-in-primary: #FFFFFF;
     --color-text-in-primary: #D4C2FF;
-    --color-text-title: #32264D;
+    --color-text-title: #bfbfbf;
     --color-text-complement: #9C98A6;
-    --color-text-base: #6A6180;
-    --color-line-in-white: #E6E6F0;
-    --color-input-background: #F8F8FC;
+    --color-text-base: #828282;
+    --color-line-in-white: #201B2D;
+    --color-input-background: #252131;
     --color-button-text: #FFFFFF;
-    --color-box-base: #FFFFFF;
-    --color-box-footer: #FAFAFC;
+    --color-box-base: var(--color-input-background);
+    --color-box-footer: var(--color-input-background);
 
     font-size: 60%;
   }
