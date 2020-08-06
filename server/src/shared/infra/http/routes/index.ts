6155bd9015a7297bc0ac41af31de51f7540833a2
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-08-06 13:39:18 -0300

    Remove routes and controller from old ConnectionsC

--- server/src/shared/infra/http/routes/index.ts
+++ server/src/shared/infra/http/routes/index.ts
@@ -1,15 +1,12 @@
 import express from 'express';
 import ClassesController from '../../../../controllers/ClassesController';
-import ConnectionsController from '../../../../controllers/ConnectionsController';
 import routesConnections from '../../../../modules/connections/infra/http/routes/routes.connections';
 
 const routes = express.Router();
 const classesController = new ClassesController();
-const connectionsController = new ConnectionsController();
 
 routes.post('/classes', classesController.create);
 routes.get('/classes', classesController.index);
 routes.use('/connections', routesConnections);
-//routes.get('/connections', connectionsController.index);
 
 export default routes;
