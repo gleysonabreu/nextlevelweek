539ce2b2565ecac3ec57fb2e27fbd304d4393dcf
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-08-07 10:09:29 -0300

    Add message errors in routes.

--- server/src/shared/infra/http/app.ts
+++ server/src/shared/infra/http/app.ts
@@ -1,14 +1,30 @@
 import "reflect-metadata";
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import '../../container';
 import '../typeorm';
 import routes from './routes';
 import cors from 'cors';
+import ApiError from "../../error/ApiError";
 
 
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(routes);
+app.use((err: Error, request: Request, response: Response, _:NextFunction) => {
+  if(err instanceof ApiError){
+    return response.status(err.statusCode).json({
+      status: 'Error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'Error',
+    message: 'Server internal error!',
+  })
+})
 
 export default app;
