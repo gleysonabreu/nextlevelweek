2bfa71481f7e54bf92e6c46da725b72a848609e4
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-08-08 10:45:51 -0300

    Fix import modules.

--- server/src/shared/infra/http/routes/index.ts
+++ server/src/shared/infra/http/routes/index.ts
@@ -1,13 +1,8 @@
 import express from 'express';
-import ClassesController from '../../../../controllers/ClassesController';
 import routesConnections from '../../../../modules/connections/infra/http/routes/routes.connections';
 import routesClasses from '../../../../modules/classes/infra/http/routes/routes.classes';
 
 const routes = express.Router();
-const classesController = new ClassesController();
-
-//routes.post('/classes', classesController.create);
-//routes.get('/classes', classesController.index);
 routes.use('/connections', routesConnections);
 routes.use('/classes', routesClasses);
 
