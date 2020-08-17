0b4b56913b26274676bde157b8c3bf2a09056917
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-08-17 12:59:28 -0300

    Add select box and DateTimePicker for filters.

--- mobileExpo/src/pages/TeacherList/index.tsx
+++ mobileExpo/src/pages/TeacherList/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, ScrollView, Text, TextInput } from 'react-native';
+import { View, ScrollView, Text, TextInput, Picker, Platform } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem, { IProffy } from '../../components/TeacherItem';
@@ -18,7 +19,9 @@ function TeacherList() {
 
   const [subject, setSubject] = useState('');
   const [week_day, setWeekDay] = useState('');
-  const [time, setTime] = useState('');
+  const [time, setTime] = useState('00:00');
+  const [showTime, setShowTime] = useState(false);
+
 
   function loadFavorites(){
     AsyncStorage.getItem('favorites').then(res => {
@@ -29,6 +32,18 @@ function TeacherList() {
       }
     })
   }
+  const onChange = async (event, selectedDate: Date) => {
+    if(event.type === 'set'){
+      const hour = selectedDate.getHours().toString();
+      const minute = selectedDate.getMinutes().toString();
+      const currentDate = `${hour}:${minute}`;
+      setShowTime(false);
+      setTime(currentDate);
+    }else{
+      setShowTime(false);
+    }
+
+  };
 
   function handleToggleFilterVisible() {
     setIsFiltersVisible(!isFiltersVisible)
@@ -70,37 +85,78 @@ function TeacherList() {
    {isFiltersVisible && ( 
     <View style={styles.searchForm}>
             <Text style={styles.label}>Matéria</Text>
-
-            <TextInput
-            placeholderTextColor="#c1bccc"
-            value={subject}
-            onChangeText={text => setSubject(text)}
+            <Picker
             style={styles.input}
-            placeholder="Qual a matéria"
-            />
+            selectedValue={subject}
+            onValueChange={(subjectValue, subjectIndex) => {
+              setSubject(subjectValue);
+            }}
+            >
+              <Picker.Item label="Selecione uma máteria" value="" />
+              <Picker.Item value='Artes' label='Artes' />
+              <Picker.Item value='Biologio' label='Biologio' />
+              <Picker.Item value='Ciências' label='Ciências' />
+              <Picker.Item value='Educação Física' label='Educação Física' />
+              <Picker.Item value='Física' label='Física' />
+              <Picker.Item value='Geografia' label='Geografia' />
+              <Picker.Item value='História' label='História' />
+              <Picker.Item value='Matemática' label='Matemática' />
+              <Picker.Item value='Português' label='Português' />
+              <Picker.Item value='Química' label='Química' />
+            </Picker>
 
             <View style={styles.inputGroup}>
               <View style={styles.inputBlock}>
                 <Text style={styles.label}>Dia da semana</Text>
-
-                <TextInput
-                placeholderTextColor="#c1bccc"
-                value={week_day}
-            onChangeText={text => setWeekDay(text)}
+                <Picker
                 style={styles.input}
-                placeholder="Qual o dia?"
-                />
+                selectedValue={week_day}
+                onValueChange={(weekDayValue, weekDayIndex) => {
+                  setWeekDay(weekDayValue);
+                }}
+                >
+                  <Picker.Item label="Selecione um dia da semana" value="" />
+                  <Picker.Item value='0' label='Domingo' />
+                  <Picker.Item value='1' label='Segunda-feira' />
+                  <Picker.Item value='2' label='Terça-feira' />
+                  <Picker.Item value='3' label='Quarta-feira' />
+                  <Picker.Item value='4' label='Quinta-feira' />
+                  <Picker.Item value='5' label='Sexta-feira' />
+                  <Picker.Item value='6' label='Sabádo' />
+                </Picker>
+
               </View>
               <View style={styles.inputBlock}>
                 <Text style={styles.label}>Horário</Text>
 
-                <TextInput
+                {/* <TextInput
+                onFocus={() => {
+                  console.log('clickei')
+                }}
                 placeholderTextColor="#c1bccc"
                 value={time}
-            onChangeText={text => setTime(text)}
                 style={styles.input}
                 placeholder="Qual o horário?"
-                />
+                /> */}
+                <RectButton
+                style={styles.input}
+                onPress={() => {
+                  setShowTime(true)
+                }}
+                >
+                  <Text>{time}</Text>
+                </RectButton>
+
+                {showTime && (
+                  <DateTimePicker
+                    value={new Date()}
+                    testID="dateTimePicker"
+                    mode="time"
+                    is24Hour
+                    display='default'
+                    onChange={onChange}
+                  />
+                )}
               </View>
    
             </View>
